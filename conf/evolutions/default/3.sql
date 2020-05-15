9b9823543c8805cd78795af8b6e94e83436919d7
Zhdanov Nikolai <nv.zhdanov@lorus-scm.com>
2020-05-15 18:49:49 +0300

    fix migration 3

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -1,9 +1,15 @@
 -- !Ups
 
-CREATE TABLE items (
-        id bigint(20) NOT NULL AUTO_INCREMENT,
-        mark_id bigint(20),
-        PRIMARY KEY (id)
+CREATE TABLE items(
+                      id              bigint(20) NOT NULL AUTO_INCREMENT,
+                      mark_id         bigint(20),
+                      model_id        bigint(20),
+                      production_date DATE,
+                      cost            real,
+                      mileage         int,
+                      PRIMARY KEY (id),
+                      FOREIGN KEY (mark_id) REFERENCES marks (id) ON DELETE SET NULL,
+                      FOREIGN KEY (model_id) REFERENCES models (id) ON DELETE SET NULL
 );
 
 -- !Downs
